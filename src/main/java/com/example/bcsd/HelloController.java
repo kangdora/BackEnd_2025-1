3102de481e3ad8958ab00d7ba4b77e49a1ffbd69
KangDora <ioprt123@gmail.com>
2025-04-30 11:20:48 +0900

    feat: introduce와 json 반환

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -1,8 +1,12 @@
 package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -16,5 +20,16 @@ public class HelloController {
     @GetMapping("/hello2")
     public String hello2() {
         return "hello";
+    }
+
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "introduce";
+    }
+
+    @GetMapping("/json")
+    public Map<String, String> getJson() {
+        return Map.of("name", "강동안", "age", "24");
     }
 }
