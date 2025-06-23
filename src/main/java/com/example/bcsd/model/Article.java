86cae02d85e35525f56580e94f8ad928ffef036a
KangDora <ioprt123@gmail.com>
2025-06-23 18:58:45 +0900

    refactor: jpa 스타일 엔티티로 수정

--- src/main/java/com/example/bcsd/model/Article.java
+++ src/main/java/com/example/bcsd/model/Article.java
@@ -1,14 +1,28 @@
 package com.example.bcsd.model;
 
-public class Article {
-    private final Long id;
-    private final String title;
-    private final Long authorId;
-    private final Long boardId;
-    private final String createdDate;
-    private final String content;
-    private final String modifiedDate;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "articles")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Long authorId;
+    private Long boardId;
+    private String createdDate;
+    private String content;
+    private String modifiedDate;
+
+    @Builder
     public Article(Long id, String title, Long authorId, Long boardId, String createdAt, String content, String modifiedDate) {
         this.id = id;
         this.title = title;
@@ -19,39 +33,9 @@ public class Article {
         this.modifiedDate = modifiedDate;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Article changeArticle(String title, String content, String modifiedDate) {
-        return new Article(id, title, authorId, boardId, createdDate, content, modifiedDate);
-    }
-
-    public Long getAuthorId() {
-        return authorId;
-    }
-
-    public Long getBoardId() {
-        return boardId;
-    }
-
-    public String getCreatedDate() {
-        return createdDate;
-    }
-
-    public String getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public Article updateModificationDate(String date) {
-        return new Article(id, title, authorId, boardId, createdDate, content, date);
-    }
-
-    public String getContent() {
-        return content;
+    public void changeArticle(String title, String content, String modifiedDate) {
+        this.title = title;
+        this.content = content;
+        this.modifiedDate = modifiedDate;
     }
 }
--- src/main/java/com/example/bcsd/model/Board.java
+++ src/main/java/com/example/bcsd/model/Board.java
@@ -1,23 +1,27 @@
 package com.example.bcsd.model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "boards")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Board {
-    private final Long id;
-    private final String title;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
 
     public Board(Long id, String title) {
         this.id = id;
         this.title = title;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
     public Board changeTitle(String title) {
         return new Board(id, title);
-    }
-
-    public Long getId() {
-        return id;
     }
 }
