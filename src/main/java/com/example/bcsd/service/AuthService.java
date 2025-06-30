e400dafb0d178683970a8ef8a5fd7cea01107604
KangDora <ioprt123@gmail.com>
2025-06-30 20:17:56 +0900

    feat: signUp 만들기

--- src/main/java/com/example/bcsd/service/AuthService.java
+++ src/main/java/com/example/bcsd/service/AuthService.java
@@ -1,12 +1,15 @@
 package com.example.bcsd.service;
 
 import com.example.bcsd.config.JwtUtil;
+import com.example.bcsd.dto.SignupRequestDto;
+import com.example.bcsd.model.USER_ROLES;
 import com.example.bcsd.model.User;
 import com.example.bcsd.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -25,5 +28,23 @@ public class AuthService {
         }
 
         return jwtUtil.createToken(user.getEmail(), user.getRoles().name());
+    }
+
+
+    @Transactional
+    public void signup(SignupRequestDto dto) {
+        if (userRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+
+        User user = User.builder()
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .phone(dto.getPhone())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .roles(USER_ROLES.USER)
+                .build();
+
+        userRepository.save(user);
     }
 }
